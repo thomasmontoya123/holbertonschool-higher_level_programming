8896b340c13fc1bed803ecb3aba761f1f0b1541d
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-17 17:15:53 -0500

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        last = 0
+        position = 0
         for colunm in row:
-            last += 1
-            if last == len(row):
-                print("{}".format(colunm), end='')
+            position += 1
+            if position != len(row):
+                print("{:d}".format(colunm), end=' ')
             else:
-                print("{}".format(colunm), end=' ')
+                print("{:d}".format(colunm), end='')
         print()
