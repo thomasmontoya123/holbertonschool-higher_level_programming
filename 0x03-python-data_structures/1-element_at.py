cabbdff6a30203613d77355cf34d9c42248d4963
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-09-17 13:46:36 -0500

    retrieves an element from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     size = len(my_list)
     if idx < 0:
         return None
-    elif idx > size:
+    elif idx >= size:
         return None
     else:
         return (my_list[idx])
