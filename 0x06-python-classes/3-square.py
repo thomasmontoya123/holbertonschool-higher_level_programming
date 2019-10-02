871d22fde15acaa0d62d89fb1ba38a075e4f2d2d
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-01 21:05:13 -0500

    area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square():
+    '''
+    Clas definition: Square
+    '''
+    def __init__(self, size=0):
+        '''
+        Args:
+            size: Square size
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._Square__size = size
+
+    def area(self):
+        return self._Square__size ** 2
