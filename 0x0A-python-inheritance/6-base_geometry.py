e4316e5ae630acd5b02fd6b06b9b234f4cf05f18
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-15 13:57:47 -0500

    Raises an Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+BaseGeometry module
+'''
+
+
+class BaseGeometry:
+    '''
+    BaseGeometry Class
+    '''
+    def area(self):
+        '''
+        empty method
+        '''
+        raise Exception("area() is not implemented")
