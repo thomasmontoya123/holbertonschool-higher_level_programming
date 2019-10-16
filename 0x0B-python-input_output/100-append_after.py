fa085caf72d4a03392b3b3cc5646d9e0b8394a16
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 18:52:18 -0500

    Advanced

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Search and update module
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''
+    Inserts a line of text to a file,
+    after each line containing a specific string (see example)
+    '''
+    with open(filename, 'r+') as f:
+        new_text = ""
+        for line in f:
+            new_text += line
+            if search_string in line:
+                new_text += new_string
+        f.write(new_text)
