81a965d4d4e780be0ea291263fb7e99b032f9a73
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 11:35:44 -0500

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+Read file modue
+'''
+
+
+def read_file(filename=""):
+    '''
+    Reads a text file (UTF8) and prints it to stdout
+
+    Parameters:
+        filename: String with the nema of the file
+    '''
+    with open(filename) as f:
+        read_data = f.read()
+        print(read_data)
