efd406eb33c2316c562cfba15b4090d3133682fc
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 18:26:53 -0500

    checker

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -23,4 +23,5 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return ({key: value for key, value in self.__dict__.items() if key in attrs})
+            return ({key: value for key,
+                    value in self.__dict__.items() if key in attrs})
