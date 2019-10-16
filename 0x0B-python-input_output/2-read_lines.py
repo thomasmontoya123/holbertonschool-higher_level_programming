34e597244505ecdc3265d2d981828c567d1cdfe4
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 13:19:48 -0500

    read n lines

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Read n lines module
+'''
+
+
+def read_lines(filename="", nb_lines=0):
+    '''
+    Reads n lines of a text file (UTF8) and prints it to stdout
+
+    Parameters:
+        filename: String with the nema of the file
+        nb_lines: number of lines to be read
+    '''
+    with open(filename) as f:
+        if nb_lines <= 0:
+            read_data = f.read()
+            print(read_data)
+        else:
+            line_count = 0
+            for line in f:
+                line_count += 1
+                print(line, end="")
+                if line_count == nb_lines:
+                    break
