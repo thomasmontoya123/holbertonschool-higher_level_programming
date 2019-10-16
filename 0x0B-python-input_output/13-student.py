2712b904040b2d69055d8d1f98849a65a1202788
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 18:28:11 -0500

    checker

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -23,7 +23,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return ({key: value for key, value in self.__dict__.items() if key in attrs})
+            return ({key: value for key,
+                    value in self.__dict__.items() if key in attrs})
 
     def reload_from_json(self, json):
         '''
