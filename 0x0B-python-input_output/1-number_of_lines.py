b2b2cf4b6d68b5aae1a3ac2f441e7fa27f760675
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-16 13:05:24 -0500

    push

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+Number of lines module
+'''
+
+
+def number_of_lines(filename=""):
+    '''
+    Returns the number of lines of a text file
+
+    Parameters:
+        filename: String with the nema of the file
+    '''
+    line_counter = 0
+    with open(filename) as f:
+        for line in f:
+            line_counter += 1
+    return line_counter
