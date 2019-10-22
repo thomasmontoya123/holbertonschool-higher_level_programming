b745f3589226c46388a636d3161443f142e7ad6d
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-10-22 14:22:07 -0500

    ifdummy for rectangle and for square

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -72,7 +72,10 @@ class Base:
         values
 
         '''
-        dummy = cls(1, 1)
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 1, 1, 1)
+        else:
+            dummy = cls(1, 1)
         dummy.update(**dictionary)
         return dummy
 
