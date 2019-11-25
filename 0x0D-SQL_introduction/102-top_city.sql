908e0f7353d5f99b6096aa733eaefa5535af2a5a
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-11-25 18:52:56 -0500

    month

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- top city 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
