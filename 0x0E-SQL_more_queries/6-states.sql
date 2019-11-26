6142b1d2358fbd7d2f17187a0f29f790988a89da
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-11-26 17:16:01 -0500

    new aproach

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,8 @@
--- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
+-- Creates the table cities .
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL ,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id)
+    REFERENCES hbtn_0d_usa.states(id),
+    name VARCHAR(256) NOT NULL,
+    )  ENGINE=INNODB;
