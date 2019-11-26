7087d5072f9001e72150b0a58d91887de6f322f8
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-11-26 17:25:18 -0500

    last

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,8 @@
--- Creates the table cities.
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUTO_INCREMENT UNIQUE PRIMARY KEY NOT NULL ,
-    state_id INT NOT NULL ,
-    FOREIGN KEY (state_id)
-    REFERENCES hbtn_0d_usa.states(id),
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT UNIQUE NOT NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    )  ENGINE=INNODB;
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
+);
