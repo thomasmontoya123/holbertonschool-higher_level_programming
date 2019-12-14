e14ef2e67e9abdb6018c5be10dcd433365f864a2
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-14 18:07:14 -0500

    "localhost" instead of ip

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 '''lists all states from the database hbtn_0e_0_usa '''
-
+from sys import argv
+import MySQLdb
 
 if __name__ == '__main__':
-    from sys import argv
-    import MySQLdb
-
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1],
+    db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("select * from states order by states.id")
