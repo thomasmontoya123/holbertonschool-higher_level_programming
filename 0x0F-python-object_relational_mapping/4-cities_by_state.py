b83983612408decacfdab696e7aef232ad9d9a6e
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-14 18:34:22 -0500

    localhost instead ip

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,12 +4,20 @@ from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="127.0.0.1", user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities  JOIN states ON cities.state_id = states.id ORDER BY cities.id;")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities"
+                " JOIN states ON cities.state_id = states.id"
+                " ORDER BY cities.id;")
+
     result = cur.fetchall()
+
     for row in result:
         print(row)
+
     cur.close()
     db.close()
