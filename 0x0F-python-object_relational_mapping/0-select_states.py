118074b76e8b82bbaa46508de77b8e5a63899f46
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-14 18:13:15 -0500

    localhost instead ip

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,10 +6,13 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", user=argv[1],
                          passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
     cur.execute("select * from states order by states.id")
     result = cur.fetchall()
+
     for row in result:
         print(row)
+
     cur.close()
     db.close()
