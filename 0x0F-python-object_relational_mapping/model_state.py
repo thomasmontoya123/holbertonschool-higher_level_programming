2848d619161d81292f12b5cf7bba4ace08a94516
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-14 19:14:21 -0500

    pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,5 +8,6 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
     __table_args__ = {'mysql_engine': 'InnoDB', 'mysql_charset': 'latin1'}
-    id = Column(Integer(), primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer(), primary_key=True,
+                nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
