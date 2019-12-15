edc85e897b93435d3ac2f0ee7b6ad1d93185053b
thomasmontoya123 <tomasmontoya123@gmail.com>
2019-12-14 19:18:17 -0500

    pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -8,7 +8,10 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
     connection = engine.connect()
     metadata = db.MetaData()
@@ -17,5 +20,6 @@ if __name__ == "__main__":
     query = db.select([states])
     ResultProxy = connection.execute(query)
     ResultSet = ResultProxy.fetchall()
+
     for row in ResultSet:
         print("{}: {}".format(row[0], row[1]))
