377ec91ae8eb39b68771486b595020787375add0
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-06 14:24:55 -0500

    peak algorithm

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def find_peak(list_of_integers):
+    if len(list_of_integers) > 0:
+        # Base case for recursion
+        if len(list_of_integers) == 1:
+            return list_of_integers[0]
+
+        middle = len(list_of_integers) // 2
+        left_list = list_of_integers[0:middle]
+
+        if len(list_of_integers) == 2:
+            if list_of_integers[middle] > list_of_integers[0]:
+                return list_of_integers[middle]
+            else:
+                return list_of_integers[0]
+
+        if (list_of_integers[middle] > list_of_integers[middle + 1] and
+                list_of_integers[middle] > list_of_integers[middle - 1]):
+            return list_of_integers[middle]
+
+        if list_of_integers[middle + 1] > list_of_integers[middle]:
+            return find_peak(list_of_integers[middle + 1:])
+
+        return find_peak(left_list)
