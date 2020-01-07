eef1ea2bc2e6c7189bebf2ee76ad8dc0eacaca2c
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-06 21:22:11 -0500

    adanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Special request.
-curl -s -d "user_id=98" -X PUT -L -H "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -d 'user_id=98' -H 'Origin: HolbertonSchool' 0.0.0.0:5000/catch_me
