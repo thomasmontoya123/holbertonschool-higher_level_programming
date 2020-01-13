fe9fd7606e884799270cc1afae7ef4081bcfb2fb
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-13 18:23:01 -0500

    len == 2 ?

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     url = 'http://0.0.0.0:5000/search_user'
 
-    if len(argv) > 1:
+    if len(argv) == 2:
         values = {'q': argv[1]}
         result = requests.post(url, data=values)
         json = result.json()
