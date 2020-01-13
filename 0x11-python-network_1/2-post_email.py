fb97592759becfa44841466e87a09f0a1730f790
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-13 15:26:42 -0500

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''displays the body of the response'''
+if __name__ == "__main__":
+	import urllib.parse
+	import urllib.request
+	from sys import argv
+
+	url = argv[1]
+	values = {'email': argv[2]}
+
+	data = urllib.parse.urlencode(values)
+	data = data.encode('utf-8')
+	req = urllib.request.Request(url, data)
+	with urllib.request.urlopen(req) as response:
+		print(response.read().decode('utf-8'))
