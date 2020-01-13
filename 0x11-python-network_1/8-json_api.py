a812b6c7e9bb6dd7607e94d89d552028bae09b6d
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-13 18:28:10 -0500

    hate on checker

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,14 +3,12 @@
 if __name__ == "__main__":
     import requests
     from sys import argv
-
-    url = 'http://0.0.0.0:5000/search_user'
-
-    if len(argv) == 2:
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) > 2:
         values = {'q': argv[1]}
         result = requests.post(url, data=values)
-        json = result.json()
         try:
+            json = result.json()
             if json:
                 print("[{}] {}".format(json.get("id"), json.get("name")))
             else:
