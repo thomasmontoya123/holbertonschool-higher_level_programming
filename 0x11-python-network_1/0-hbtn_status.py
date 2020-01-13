ead28c1f4f9c382ff4238a8a2adc2b096aca2cd1
thomasmontoya123 <tomasmontoya123@gmail.com>
2020-01-13 14:46:06 -0500

    tab size fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,10 @@
 
 if __name__ == "__main__":
     import urllib.request as urlib
+
     with urlib.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print("    - type: {}".format(type(html)))
+        print("    - content: {}".format(html))
+        print("    - utf8 content: {}".format(html.decode('utf-8')))
