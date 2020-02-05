7718c0f485e8c6aa54e68f134fe2252532da3551
Thomas <tomasmontoya123@gmail.com>
2020-02-04 20:36:46 -0500

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 let x = '';
+let i;
 if (!parseInt(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (i = 0; i < process.argv[2]; i++) {
     x = '';
     for (let j = 0; j < process.argv[2]; j++) {
       x += 'X';
