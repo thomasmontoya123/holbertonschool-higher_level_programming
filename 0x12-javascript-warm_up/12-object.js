793ceb8e3a273f468d3a06e820114418313c8f59
Thomas <tomasmontoya123@gmail.com>
2020-02-04 21:11:17 -0500

    semistandard fix

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const obj = {
-    type: 'object',
-    value: 12
-  };
-  console.log(obj);
-  obj.value = 89;
-  console.log(obj);
+  type: 'object',
+  value: 12
+};
+console.log(obj);
+obj.value = 89;
+console.log(obj);
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (parseInt(process.argv[2]) && parseInt(process.argv[3])) {
-    console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
-  } else {
-    console.log('NaN');
-  }
-  function add (a, b) {
-    return a + b;
-  }
+  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
+} else {
+  console.log('NaN');
+}
+function add (a, b) {
+  return a + b;
+}
