75b5e6035ffc6f8f9a392c2e50db3f510fa7717f
Thomas <tomasmontoya123@gmail.com>
2020-02-10 20:29:42 -0500

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let x = '';
+      for (let j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+
+  rotate () {
+    const widthHolder = this.width;
+    this.width = this.height;
+    this.height = widthHolder;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
