f0d3dba8732cce514d889ca3a7673452ebe70492
Thomas <tomasmontoya123@gmail.com>
2020-02-11 17:50:17 -0500

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(process.argv[3], body, (err) => {
+      if (err) throw err;
+    });
+  }
+});
