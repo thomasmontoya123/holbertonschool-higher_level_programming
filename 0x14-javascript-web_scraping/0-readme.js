0efe6214b7e7c66585ea617a71756a20f456a3fa
Thomas <tomasmontoya123@gmail.com>
2020-02-11 13:38:43 -0500

    else

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+fs.readFile(process.argv[2], function (err, data) {
   if (err) {
     return console.error(err);
+  } else {
+    console.log(data.toString());
   }
-  console.log(data.toString());
 });
