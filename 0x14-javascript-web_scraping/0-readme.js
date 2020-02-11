5f89cab016f5079c1cffb8417cb324326a238fe8
Thomas <tomasmontoya123@gmail.com>
2020-02-11 13:33:02 -0500

    Task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/local/bin/node
+const fs = require('fs');
+
+fs.readFile(process.argv[2], function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data.toString());
+});
